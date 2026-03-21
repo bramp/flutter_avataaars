8b196bc2c7793702806303afc2cddcdec9798350
bramp <bramp@users.noreply.github.com>
2026-03-21 16:58:34 +0000

    deploy: d0f86232e043a89a0b67e7f5a14e111961714d36

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2830174621" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3877673065" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
