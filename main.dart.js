1e63dcce2f9efc3c49ace9dc32d5a688ddb3ec14
bramp <bramp@users.noreply.github.com>
2026-04-04 00:11:00 +0000

    deploy: 017e4c2190a4734554f01e9edb4b1c66b3c18c18

--- main.dart.js
+++ main.dart.js
@@ -24477,7 +24477,7 @@ $iGr:1}
 A.agi.prototype={
 $1(a){var s=A.cD().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/052f31d115eceda8cbff1b3481fcde4330c4ae12/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb/":s)+a},
 $S:159}
 A.GI.prototype={
 gn(a){var s=this.a
